47574b88cfaba4732cdbe4571891a38093d4575a
Alex <aleksandrekiknadzee@gmail.com>
2024-02-06 11:33:55 +0400

    fix: move movie intro logic from App to the MovieIntro component

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,13 @@
-import { useState, useEffect, useRef } from "react";
-import useFetch from "./hooks/useFetch.jsx";
 import Header from "./components/Header/Header.jsx";
 import MovieIntro from "./components/MovieIntro/MovieIntro.jsx";
 import FeaturedCards from "./components/Cards/FeaturedCards.jsx";
-import Footer from "./components/Footer/Footer.jsx"
+import Footer from "./components/Footer/Footer.jsx";
 function App() {
-  const [moviesIntro, setMoviesIntro] = useState([]);
-  const [currentIntro, setCurrentIntro] = useState([]);
-  const { get, loading } = useFetch("https://api.themoviedb.org/3");
-  const intervalRef = useRef(0);
-  let introTimeoutId;
-
-  useEffect(() => {
-    get("/movie/upcoming?language=en-US&page=1")
-      .then((data) => {
-        const movies = data.results.map((item) => item).splice(0, 5);
-        setCurrentIntro(movies[0]);
-        setMoviesIntro(movies);
-        startIntroInterval(movies);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
-  useEffect(() => {
-    startIntroInterval(moviesIntro);
-  }, [currentIntro]);
-
-  const startIntroInterval = (movies) => {
-    introTimeoutId = intervalRef.current;
-    clearInterval(introTimeoutId);
-    introTimeoutId = setInterval(() => {
-      const currentIndex = movies.indexOf(
-        movies.find((movie) => currentIntro.id === movie.id)
-      );
-      if (currentIndex !== movies.length - 1) {
-        setCurrentIntro(movies[currentIndex + 1]);
-      } else {
-        setCurrentIntro(movies[0]);
-      }
-    }, 5_000);
-    intervalRef.current = introTimeoutId;
-  };
-  const onClickIntro = (index) => {
-    setCurrentIntro(moviesIntro[index]);
-  };
   return (
     <>
       <Header />
       <main className="">
-        <MovieIntro
-          loadin={loading}
-          handleIntroClick={onClickIntro}
-          currentIntro={currentIntro}
-          moviesIntro={moviesIntro}
-        />
+        <MovieIntro />
         <FeaturedCards
           type={"movie"}
           category={"now_playing"}
--- src/components/MovieIntro/MovieIntro.jsx
+++ src/components/MovieIntro/MovieIntro.jsx
@@ -1,15 +1,53 @@
+import { useState, useEffect, useRef } from "react";
 import MovieDescription from "./MovieDescription";
 import MovieIntroCarousel from "./MovieIntroCarousel";
+import useFetch from "../../hooks/useFetch";
 
-export default function MovieIntro({
-  handleIntroClick,
-  currentIntro,
-  moviesIntro,
-  loading
-}) {
+export default function MovieIntro() {
+  const [moviesIntro, setMoviesIntro] = useState([]);
+  const [currentIntro, setCurrentIntro] = useState([]);
+  const { get, loading } = useFetch("https://api.themoviedb.org/3");
+  const intervalRef = useRef(0);
+  let introTimeoutId;
+
+  useEffect(() => {
+    get("/movie/upcoming?language=en-US&page=1")
+      .then((data) => {
+        const movies = data.results.map((item) => item).splice(0, 5);
+        setCurrentIntro(movies[0]);
+        setMoviesIntro(movies);
+        startIntroInterval(movies);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    startIntroInterval(moviesIntro);
+  }, [currentIntro]);
+
+  const startIntroInterval = (movies) => {
+    introTimeoutId = intervalRef.current;
+    clearInterval(introTimeoutId);
+    introTimeoutId = setInterval(() => {
+      const currentIndex = movies.indexOf(
+        movies.find((movie) => currentIntro.id === movie.id)
+      );
+      if (currentIndex !== movies.length - 1) {
+        setCurrentIntro(movies[currentIndex + 1]);
+      } else {
+        setCurrentIntro(movies[0]);
+      }
+    }, 5_000);
+    intervalRef.current = introTimeoutId;
+  };
+  const onClickIntro = (index) => {
+    setCurrentIntro(moviesIntro[index]);
+  };
   return (
     <>
-    {!loading && (
+      {!loading && (
         <img
           src={`https://image.tmdb.org/t/p/original/${currentIntro.poster_path}`}
           alt={`${currentIntro.title}`}
@@ -18,14 +56,14 @@ export default function MovieIntro({
           height="750"
         />
       )}
-    <div className="container flex justify-between items-center h-screen text-white">
-      {currentIntro && <MovieDescription currentIntro={currentIntro} />}
-      <MovieIntroCarousel
-        handleIntroClick={handleIntroClick}
-        currentIntro={currentIntro}
-        moviesIntro={moviesIntro}
-      />
-    </div>
+      <div className="container flex justify-between items-center h-screen text-white">
+        {currentIntro && <MovieDescription currentIntro={currentIntro} />}
+        <MovieIntroCarousel
+          handleIntroClick={onClickIntro}
+          currentIntro={currentIntro}
+          moviesIntro={moviesIntro}
+        />
+      </div>
     </>
   );
 }
